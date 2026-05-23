804f6ca0fe0685f07da82fa4c8a3cd4411afb5f6
7uminalnteractive <lpestudioscontato@gmail.com>
2026-05-23 04:34:48 -0300

    Criar auth.js

---
+++ auth.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://tqsalhscgkepttbczyjq.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Q99EhX_HpUVotGGqmWAf4A_pkiTB7bK";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
