6dfe08f6a851e42e51f36f310c421c0e96f154f2
7uminalnteractive <lpestudioscontato@gmail.com>
2026-05-24 20:00:12 -0300

    Atualizar o auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,37 @@
 const SUPABASE_URL = "https://tqsalhscgkepttbczyjq.supabase.co";
 const SUPABASE_KEY = "sb_publishable_Q99EhX_HpUVotGGqmWAf4A_pkiTB7bK";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+async function atualizarNavbar() {
+
+  const { data: { user } } =
+    await supabaseClient.auth.getUser();
+
+  const contaLink =
+    document.getElementById("contaLink");
+
+  if (!contaLink) return;
+
+  if (user) {
+
+    contaLink.innerText = "Minha conta";
+    contaLink.href = "minha-conta.html";
+
+  } else {
+
+    contaLink.innerText = "Login";
+    contaLink.href = "login.html";
+
+  }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  atualizarNavbar();
+
+});
